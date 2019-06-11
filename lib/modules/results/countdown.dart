52e534bad4259996f7a5ba69988a64c1b4a0e77d
Kyru Cabading <kyrusri@gmail.com>
2019-06-12 02:28:37 +0800

    fix(countdown): use correct logic in resetting timer

--- lib/modules/results/countdown.dart
+++ lib/modules/results/countdown.dart
@@ -3,22 +3,23 @@ import 'package:intl/intl.dart';
 import 'package:lotto/styles.dart';
 
 class CountDownTimer extends StatelessWidget {
-  final now = DateTime.now();
+  final _now = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    final todayAt12 = DateTime(now.year, now.month, now.day, 23, 30);
-    final isPastTargetTime = now.isAfter(todayAt12);
-    final targetTime =
-        isPastTargetTime ? todayAt12.add(Duration(days: 1)) : todayAt12;
+    final schedule = DateTime(_now.year, _now.month, _now.day, 22, 00);
 
     return StreamBuilder(
         stream: Stream.periodic(Duration(seconds: 1), (i) => i),
         builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+          final targetTime = DateTime.now().isAfter(schedule)
+              ? schedule.add(Duration(days: 1))
+              : schedule;
+
           DateFormat format = DateFormat("mm:ss");
-          int now = DateTime.now().millisecondsSinceEpoch;
+          int _now = DateTime.now().millisecondsSinceEpoch;
           Duration remaining =
-              Duration(milliseconds: targetTime.millisecondsSinceEpoch - now);
+              Duration(milliseconds: targetTime.millisecondsSinceEpoch - _now);
           var dateString =
               '${remaining.inHours}:${format.format(DateTime.fromMillisecondsSinceEpoch(remaining.inMilliseconds))}';
           return Container(
@@ -27,7 +28,7 @@ class CountDownTimer extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  'time till next update'.toUpperCase(),
+                  'Time till next results'.toUpperCase(),
                   style: Styles.countdownSubtitle,
                 ),
                 Text(
